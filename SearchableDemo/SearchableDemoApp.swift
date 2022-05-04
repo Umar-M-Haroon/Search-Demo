c85b22672c4e713a7c5e353b700e8dcf88ee0ec3
Umar Haroon <umar.m.haroon@gmail.com>
2022-05-03 19:46:22 -0600

    Initial Commit

---
+++ SearchableDemo/SearchableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableDemoApp.swift
+//  SearchableDemo
+//
+//  Created by Umar Haroon on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
